9c091b8e07746e0d97e3d9d6527e38062eede9c0
Tabea Haverkamp <tabea@haverkamp-os.de>
2020-10-15 22:00:26 +0000

    working setup by calling ssc-cloudinit

--- orchestration/set_up.sh
+++ orchestration/set_up.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-openstack stack create group4_stack -f 'yaml' -t /home/ubuntu/ACC-Labs/Lab3/orchestration/master_vm.yaml 
+openstack stack create group4_stack -f 'yaml' -t /home/ubuntu/ACC-Project-BENCHOP/orchestration/master_vm.yaml 
--- orchestration/slave_vm.sh
+++ orchestration/slave_vm.sh
@@ -15,7 +15,7 @@ sudo -H pip3 install celery
 sudo -H pip3 install Flask
 
 echo "Installing Octave and converter.."
-sudo -H apt-get install octave
+sudo -H apt-get install -y octave
 sudo -H pip3 install oct2py
 
 
@@ -27,3 +27,9 @@ sudo git clone https://github.com/TabeaHaverkamp/ACC-Project-BENCHOP.git
 echo "Setting permission"
 sudo chown -R ubuntu.users /home/ubuntu/ACC-Project-BENCHOP
 
+sudo sed 's/localhost/192.168.2.146/' -i /home/ubuntu/ACC-Project-BENCHOP/BENCHOP/celery_app.py  
+##TODO: REPLACE 192.168.2.146 WITH PRIVATE IP
+
+echo "starting celery"
+cd /home/ubuntu/ACC-Project-BENCHOP/BENCHOP 
+sudo screen -S celeryserver -d -m bash -c 'cd /home/ubuntu/ACC-Project-BENCHOP/BENCHOP/ && celery -A tasks worker --loglevel=INFO -n worker1@%h --concurrency=1 --max-memory-per-child 100'
