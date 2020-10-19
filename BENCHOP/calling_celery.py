d46f299db11dec657b194b29ad8c81efe7dfb3d1
Tabea Haverkamp <tabea@haverkamp-os.de>
2020-10-19 09:41:02 +0000

    flask app runs, add comments

--- BENCHOP/calling_celery.py
+++ BENCHOP/calling_celery.py
@@ -1,4 +1,5 @@
 from tasks import singleMethod
+
 from celery_app import celery_app
 from celery import group
 from celery.result import AsyncResult
@@ -7,7 +8,6 @@ from flask import Flask, jsonify, render_template, request
 import pygal, json
 from pygal.style import Style
 from pygal.style import CleanStyle, DefaultStyle
-start = time.time()
 
 #problems stated in Table.m
 ALL_PROBLEMS = ['P1aI', 'P1bI', 'P1cI', 'P1aII']
@@ -15,22 +15,24 @@ ALL_METHODS = ['MC','MC-S','QMC-S','MLMC','MLMC-A','FFT','FGL','COS','FD','FD-NU
 #but for testing puroses, only use subset of methods and problems
 METHODS = ['COS', 'UniformGrid']
 PROBLEMS = ['P1aI', 'P1bI']
-#Start benchmarking tasks
 ALL_RESULTS = {}
-for prob in PROBLEMS:
-	ALL_RESULTS[prob] = {}
-	for method in METHODS:
-		result = singleMethod.delay(prob, method)
-		ALL_RESULTS[prob][method] = {'state': 'PENDING', 'id': result.id, 'result': {'time': 0, 'relerr': 0}}
 
-#Use this to update state
+#Start benchmarking tasks. Setting up with the given methods / problems
+def setup(problems, methods):
+	global ALL_RESULTS
+	for prob in problems:
+		ALL_RESULTS[prob] = {}
+		for method in methods:
+			result = singleMethod.delay(prob, method)
+			ALL_RESULTS[prob][method] = {'state': 'PENDING', 'id': result.id, 'result': {'time': 0, 'relerr': 0}}#Use this to update state
+	
+# update the results in ALL_RESULTS for giving those to the interface
 def update_results():
+	global ALL_RESULTS
 	for prob in ALL_RESULTS.keys():
 		for method in ALL_RESULTS[prob].keys():
 			if ALL_RESULTS[prob][method]['state'] == 'PENDING':
 				result = AsyncResult(ALL_RESULTS[prob][method]['id'], app=celery_app)
-				while result.state != 'SUCCESS':
-					time.sleep(1)
 				if result.state == 'SUCCESS':
 					ALL_RESULTS[prob][method]['state'] = result.state
 					ALL_RESULTS[prob][method]['result']['time'] = result.result[0]
@@ -38,20 +40,11 @@ def update_results():
 
 
 
-
-#Start the flask app after all tasks are created
-flask_app = Flask(__name__)
-
-#Updates global variable with newly finished results and returns everything
-@flask_app.route('/benchmark', methods=['GET'])
-def obtain_benchmark_results():
-	global ALL_RESULTS
-	update_results()
-	
+def plot(all_results):
 	time_charts = []
 	relerr_charts = []
 	print("start rendering..")
-	for prob in sorted(ALL_RESULTS.keys()):
+	for prob in sorted(all_results.keys()):
 		time_chart = pygal.Bar(style=CleanStyle)
 		time_chart.title = "Time to solve problem: " + prob
 		time_chart.y_title = "Execution time [s]"
@@ -63,14 +56,14 @@ def obtain_benchmark_results():
 		labels = []
 		time_data = []
 		relerr_data = []
-		for method in ALL_RESULTS[prob].keys():
-			if ALL_RESULTS[prob][method]['state'] == 'SUCCESS':
+		for method in all_results[prob].keys():
+			if all_results[prob][method]['state'] == 'SUCCESS':
 				labels.append(method)
 			else:
 				labels.append(method + " PENDING")
 
-			time_data.append(ALL_RESULTS[prob][method]['result']['time'])
-			relerr_data.append(ALL_RESULTS[prob][method]['result']['relerr'])
+			time_data.append(all_results[prob][method]['result']['time'])
+			relerr_data.append(all_results[prob][method]['result']['relerr'])
 
 		time_chart.x_labels = map(str, labels)
 		time_chart.add('Time', time_data)
@@ -81,16 +74,73 @@ def obtain_benchmark_results():
 		relerr_chart.add('Relerr', relerr_data)
 		relerr_chart_data = relerr_chart.render_data_uri()
 		relerr_charts.append(relerr_chart_data)
+	return time_charts, relerr_charts
+
+
+
+#Start the flask app after all tasks are created
+flask_app = Flask(__name__)
+
+@flask_app.route('/tab', methods=['GET'])
+def just_try():
+	start = time.time()
+	job = []
+	ar = {}
+	for prob in PROBLEMS:
+		for method in METHODS:
+			job.append(singleMethod.delay(prob, method))
+	for j in job:
+		result = j.get()
+		print(j)
+	print(f"\n\n\n ---- \n\n this took {time.time() - start}sec")
+
+
+
+#Updates global variable with newly finished results and returns everything
+@flask_app.route('/benchmark', methods=['GET'])
+def obtain_benchmark_results():
+	global ALL_RESULTS, PROBLEMS, METHODS
+	print(PROBLEMS)
+	setup(PROBLEMS, METHODS)
+	update_results()
+	time_charts, relerr_charts =  plot(ALL_RESULTS)
 	print("done rendering...")
 	return render_template("webui.html", results = ALL_RESULTS, time_charts = time_charts, relerr_charts = relerr_charts)
+	#"check the website\n"
 
-
+# print ALL_RESULTS in json format
 @flask_app.route('/benchmark/json', methods=['GET'])
 def obtain_results_json():
 	global ALL_RESULTS
-	update_results()
+	if ALL_RESULTS == {}: # if not called before set up beforehand.
+		setup(PROBLEMS, METHODS)
+	update_results(PROBLEMS, METHODS)
 	return jsonify(ALL_RESULTS)
 
-	
+
+# call one specific problem with web interface
+@flask_app.route('/<problem>/<method>', methods=['GET'])
+def obtain_specific_problem(problem, method):
+	result = singleMethod.delay(problem, method)
+	results = {'state': 'PENDING', 'id': result.id, 'result': {'time': 0, 'relerr': 0}}
+
+	time_charts, relerr_charts =  plot(all_results)
+	return render_template("webui.html", results = results, time_charts = time_charts, relerr_charts = relerr_charts)
+
+# call one specific problem with JSON output
+@flask_app.route('/benchmark/json/<problem>/<method>', methods=['GET'])
+def obtain_specific_problem_json(problem, method):
+	result = singleMethod.delay(problem, method)
+	results = {'state': 'PENDING', 'id': result.id, 'result': {'time': 0, 'relerr': 0}}
+	if results['state'] == 'PENDING':
+		result = AsyncResult(results['id'], app=celery_app)
+		time.sleep(1)
+		if result.state == 'SUCCESS':
+			results['state'] = result.state
+			results['result']['time'] = result.result[0]
+			results['result']['relerr'] = result.result[1]
+	return jsonify(results)
+
+
 if __name__ == '__main__':
     flask_app.run(host='0.0.0.0', port = 50070, debug=True)
